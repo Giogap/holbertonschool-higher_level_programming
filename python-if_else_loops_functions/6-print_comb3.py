bc84189a46d73c6079a18b6eec34322982d74f96
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-22 23:56:43 -0500

    update 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+cont = 0;
+j = 0
+for i in range(0, 9):
+    cont = cont + 1
+    j = cont
+    for j in range (j, 10):
+        if i != j:
+            print("{}{}".format(i, j), end=", ")
+    
+
