924b9c3696e731f2a36308c4c0bd130bac51d6af
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-24 19:20:06 -0500

    update 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == '__main__':
     n = len(fun)
     if n == 1:
         print("{} arguments.".format(n - 1))
+    elif n == 2:
+        print("{} argument:".format(n - 1))
+        print("{}: {}".format(n - 1, fun[1]))
     else:
         print("{} arguments:".format(n - 1))
         for i in range(1, n):
