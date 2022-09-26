2ccfc039c820c3fa5e17f14518c9d380725fcf75
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-25 23:58:48 -0500

    update 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    cp_list = my_list.copy()
     if idx < 0:
-        return my_list
+        return cp_list
     elif idx >= len(my_list):
-        return my_list
+        return cp_list
     else:
-        my_list[idx] = element
-        return my_list
+        cp_list[idx] = element
+        return cp_list
