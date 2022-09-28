c11ee4f035c78f499541d3090a35e109bd9bc497
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-27 22:27:01 -0500

    update 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-Prototype: def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key, value})
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary.update({key: value})
     return a_dictionary
