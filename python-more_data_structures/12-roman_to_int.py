be2e7495efd9420fde32614002953aa354586a13
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-29 00:53:41 -0500

    update 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string = "":
+        return 0
     dic_r = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     acum = 0
     for i in range(len(roman_string)):
