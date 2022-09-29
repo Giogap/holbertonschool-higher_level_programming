65605f3501232d89329ffd71c3821d7ac1da4a78
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-29 18:05:39 -0500

    update 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,10 @@
 def roman_to_int(roman_string):
     dic_r = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     acum = 0
-    if roman_string == "":
-        return 0
+    romano = list(roman_string)
     for i in range(len(roman_string)):
-        if i > 0 and dic_r[roman_string[i]] > dic_r[roman_string[i - 1]]:
-            acum += dic_r[roman_string[i]] - 2
+        if i > 0 and dic_r[romano[i]] > dic_r[romano[i - 1]]:
+            acum += dic_r[romano[i]] - 2 
         else:
-            acum += dic_r[roman_string[i]]
+            acum += dic_r[romano[i]]
     return acum
