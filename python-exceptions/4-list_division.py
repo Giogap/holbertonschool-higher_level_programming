521be886280da782304825136bfad601009552b8
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-30 18:33:13 -0500

    update 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     length = [0] * list_length
-    for i in range(length):
+    for i in range(list_length):
         try:
             length[i] = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
