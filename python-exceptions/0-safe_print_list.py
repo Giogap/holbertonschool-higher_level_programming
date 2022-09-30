e7f4e47b0d323cdbc21b9da8d3ca2bd6ef4a0e9b
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-30 16:31:33 -0500

    update 1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,10 +2,6 @@
 def safe_print_list(my_list=[], x=0):
     cont = 0
     for i in my_list[:x]:
-        try:
             print(i, end="")
             cont += 1
-        except IndexError:
-            print()
-    print()
     return cont
