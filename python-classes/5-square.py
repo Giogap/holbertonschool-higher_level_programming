8d1831f4fd0dee947193458a5c2200d4ea950509
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-01 17:34:17 -0500

    update 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
-     def area(self):
+    def area(self):
         area = (self.__size) * (self.__size)
         return area
 
@@ -24,4 +24,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
