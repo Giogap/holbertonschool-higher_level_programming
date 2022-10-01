45036b74b621a716d25a42e5662723ac6ee4ec5a
Giogap <giogap@P-GIOVANNI.localdomain>
2022-09-30 21:10:10 -0500

    update 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+
+""" Class Square """
+
+
+class Square:
+    """ create Square size private """
+    def __init__ (self, size=0):
+        self.__size = size
