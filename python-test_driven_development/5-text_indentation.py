03b101d9f5c0a2940ccbdedaa137d80bc1e67ac7
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-04 16:28:32 -0500

    update 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,10 +9,15 @@ def text_indentation(text):
     """ function that prints a text with 2 new lines """
     if type(text) is not str:
         raise TypeError("text must be a string")
-    for char in text:
-        if char == '.' or char == '?' or char == ':':
-            print(char)
+    after_new_line = False
+    for c in text:
+        if after_new_line:
+            if c == " ":
+                continue
+            after_new_line = False
+        if c == '.' or c == '?' or c == ':':
+            print(c)
             print("")
+            after_new_line = True
         else:
-            print(char, end="")
-
+            print(c, end="")
