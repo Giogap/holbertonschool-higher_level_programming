65e610a27415dfe47eaefd163425807d278c0506
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-05 18:52:29 -0500

    upload 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ Define class rectangle
 class Rectangle:
     """ Rectangle  """
     def __init__(self, width = 0, height = 0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
