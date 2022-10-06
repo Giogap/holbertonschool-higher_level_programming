fb2ad8c912252b377b20cb3bea0cfb88e906c55c
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-05 19:33:41 -0500

    upload 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" This is empty class """
+
+
+class Rectangle:
+    """ create rectangle """
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        return (2 * (self.__width + self.__height))
