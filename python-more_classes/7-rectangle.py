63346a6371ad16a695d752cf13076ed2c8877a49
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-07 16:23:56 -0500

    update 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-""" Rectangle Class """
+""" This is empty class """
 
 
 class Rectangle:
-    """ Defines a rectangle """
+    """ create rectangle """
+
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
 
-    def __init__(self, width = 0, height = 0):
-        self.width = width
+    def __init__(self, width=0, height=0):
         self.height = height
+        self.width = width
         Rectangle.number_of_instances += 1
-
-    @property
-    def width(self):
-        return self.__width
 
     @property
     def height(self):
         return self.__height
 
+    @property
+    def width(self):
+        return self.__width
+
     @width.setter
-    def width(self, value):
-        if type(value) is not int:
+    def width(self, width):
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self.__width = width
 
     @height.setter
     def height(self, height):
@@ -47,17 +48,15 @@ class Rectangle:
         return perimeter
 
     def __str__(self):
-        string = ''
-        for i in range(self.__height):
-            for j in range(self.__width):
-                string += str(self.print_symbol)
-            string += "\n"
-        return string[:-1]
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return '\n'.join(str(self.print_symbol) * self.__width for _ in range(
+            self.__height))
 
     def __repr__(self):
-        return ('Rectangle(' + str(self.__width) + ', ' +
-                str(self.__height) + ')')
+        return "Rectangle({}, {})".format(
+            eval(str(self.__width)), eval(str(self.__height)))
 
     def __del__(self):
-        print('Bye rectangle...')
+        print("bye rectangle...")
         Rectangle.number_of_instances -= 1
