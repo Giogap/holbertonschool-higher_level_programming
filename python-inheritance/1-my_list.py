51f00ae23ee2b53a8d663f4b6f1779556df23fdf
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-12 18:02:48 -0500

    upload 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,5 +5,4 @@
 class MyList(list):
     """Class MyList that inherits from list"""
     def print_sorted(self):
-        """Print list sorted"""
-        print (sorted(self))
+        print(sorted(self))
