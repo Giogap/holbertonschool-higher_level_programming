2f2b1807470682ae0831b68688e17017e11e66ca
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-12 18:58:58 -0500

    upload 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Same class or inherit from  """
+
+
+def is_kind_of_class(obj, a_class):
+    """ object is an instance of instance class  """
+    return type(obj) is a_class
