d53e3d22c1967912cecdee5761e17e39ccd87ff6
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-13 18:01:05 -0500

    upload 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Full rectangle"""
+
+
+class BaseGeometry:
+    """ empty class """
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        super().__init__()
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
