dbe8f975ee37156a15669a4c81d21c6cbd6d6d8c
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-12 20:21:59 -0500

    update 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """the object is instance of, or the object is instance of a class """
+    return (isinstance(obj, a_class))
