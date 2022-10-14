9de55c0f7c491df2ff822e94cde43a80ba628bca
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-14 12:35:28 -0500

    upload 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Read file"""
+
+
+def read_file(filename=""):
+    """function that reads a text file"""
+    with open("my_file_0.txt", encoding='utf-8') as file:
+        print(file.read())
+
