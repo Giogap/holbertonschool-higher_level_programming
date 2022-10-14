11f685eb837ffd9b3d0d7a7e0c888cb87ac1ad70
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-13 19:27:35 -0500

    upload 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Square #2"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square that inherits from Rectangle"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Area"""
+        return self.size ** 2
+
+    def __str__(self):
+        """should print, and str() should return"""
+        return "[Square] {}/{}".format(self.size, self.size)
