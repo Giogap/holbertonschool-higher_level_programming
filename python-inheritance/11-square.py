d19a23edddf0796e35eca9ed0868d854caffecfd
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-13 20:03:39 -0500

    upload 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
-"""Square #2"""
-Rectangle = __import__("9-rectangle").Rectangle
+""" Doc """
+
+
+class BaseGeometry:
+    """ empty class """
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Doc """
+    def __init__(self, width, height):
+        """ Doc """
+        super().__init__()
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
 
 class Square(Rectangle):
-    """Square that inherits from Rectangle"""
+    """ Doc """
     def __init__(self, size):
-        """Instantiation with size"""
-        self.integer_validator('size', size)
+        self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """Area"""
-        return self.__size ** 2
+        return self.__size * self.__size
 
     def __str__(self):
-        """should print, and str() should return"""
-        return ("[Square]" + str(self.__size) + "/" + str(self.__size))
+        return "[Square] {}/{}".format(self.__size, self.__size)
