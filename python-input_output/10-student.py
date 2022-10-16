1906e26841d0e73d4706c2d9eeb2d5ade7d12227
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 <giogap@gmail.com>
2022-10-16 17:22:21 -0500

    upload 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Student to JSON with filter"""
+""" Class Student """
 
 
 class Student:
-    """class Student that defines a student"""
-
+    """ tudent that defines a student """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if type(attrs) != list:
+        if attrs is None:
             return self.__dict__
+        new_dict = {}
+        for i in attrs:
+            try:
+                new_dict[i] = self.__dict__[i]
+            except:
+                pass
+        return new_dict
