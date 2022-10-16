3ff002bd81d2eaacebb401ab0812656bc3dae42d
Giogap <giogap@P-GIOVANNI.localdomain>
2022-10-15 19:52:46 -0500

    upload 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""""""
+"""Load, add, save"""
 from sys import argv
 
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-file_name = 'add_item.json'
+file_name = "add_item.json"
 
 try:
     current_json = load_from_json_file(file_name)
