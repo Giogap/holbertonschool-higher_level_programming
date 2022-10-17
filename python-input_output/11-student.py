db7b10eb9cea5b3d245084fa43d7805bdc7f91cc
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 <giogap@gmail.com>
2022-10-16 19:39:08 -0500

    upload 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Student to disk and reload"""
+
+
+class Student:
+    """Class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            nd = {}
+            for i in attrs:
+                for key, value in self.__dict__.items():
+                    if key == i:
+                        nd[key] = value
+            return nd
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.__dict__.update(json)
