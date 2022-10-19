4a008f5113c7e82c16976d3250df37ee0c9e56a2
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 <giogap@gmail.com>
2022-10-19 10:39:00 -0500

    upload 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ class Base """
 
-__nb_objects = 0
 
 
 class Base:
     """ Doc """
+    __nb_objects = 0
+    
     def __init__(self, id=None):
         if id is not None:
             self.id = id
