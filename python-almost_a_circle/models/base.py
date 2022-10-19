2b61c1a0081821a60f52f221e9f21ca922591146
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 <giogap@gmail.com>
2022-10-19 10:41:31 -0500

    upload 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,11 +6,11 @@
 class Base:
     """ Doc """
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             self.id = id
-            __nb_objects += 1
-            id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
