67380014ec6ad820b50d1f808fd245e094d52252
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 <giogap@gmail.com>
2022-10-19 18:09:47 -0500

    upload 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,9 +75,7 @@ class Rectangle(Base):
             for width in range(self.__width):
                 print('#', end='')
             print()
-        
+
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
-
-
