6bda4a020c1088300d21e32b560eb29f4012c940
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 <giogap@gmail.com>
2022-10-21 15:07:03 -0500

    update 18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,52 +1,50 @@
 #!/usr/bin/python3
-""" class Base """
-
+"""
+class base
+"""
 
 import json
 
 
-
 class Base:
     """ Doc """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
-            self.id = id
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Dictionary to JSON string"""
-        if list_dictionaries is Empty or list_dictionaries is None:
+        """ return JSON string of list dictionaries """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """"""
+        """writes the JSON string representation"""
         if list_objs is not None:
-            list_objs = [i.to_dictionary() for i in list_objs]
+            list_objs = [n.to_dictionary() for n in list_objs]
         filename = cls.__name__ + ".json"
-        with open(filename, "w", encoding="utf-8") as file:
-            file.write(cls.to_json_string(list_objs))
+        with open(filename, "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string is None or json_string is Empty:
+        if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
 
     @classmethod
-    def update(cls, **dictionary):
-        if cls.__name__ == "Square":
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
         elif cls.__name__ == "Square":
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
-        
