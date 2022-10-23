adc8214225a1c1263ac54e0c88a25ad45bcec60b
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 <giogap@gmail.com>
2022-10-23 18:57:00 -0500

    upload test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -7,10 +7,17 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     """Test of Base() for assigning automatically an ID """
-    def testbase(self):
+    def testBase(self):
         b1 = Base(2)
         self.assertAlmostEqual(b1.id, 2)
 
-    def testbaseNone(self):
+    def testBaseNone(self):
         b1 = Base()
-        self.assertAlmostEqual(b1.id, 1)
+        self.assertEqual(b1.id, 1)
+
+    def testBaseJsonStr(self):
+        self.assertIsNotNone(Base.to_json_string)
+
+    def testBaseJsonStrNone(self):
+        test = Base.to_json_string(None)
+        self.assertEqual(test, "[]")
