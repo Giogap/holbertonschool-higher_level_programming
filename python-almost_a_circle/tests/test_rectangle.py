0b7b0b1a77f1841b460ca535c2e615c165254f1c
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 <giogap@gmail.com>
2022-10-23 19:49:41 -0500

    update test

---
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Test base """
+
+import unittest
+
+from models.rectangle import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    """Test of Rectangle()"""
+    def testRectangle(self):
+        r = Rectangle(1, 2)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        
