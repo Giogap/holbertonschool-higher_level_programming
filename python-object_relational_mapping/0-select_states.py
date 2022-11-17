ba4f4dcef0fb017f5c2b212957d49683585a6a87
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 <giogap@gmail.com>
2022-11-17 16:48:56 -0500

    upload 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+def mysqlconnect():
+    """"""
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password="",
+        database=argv[3],
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    rows = cursor.fetchall()
+    for col in rows:
+        print(col)
+
+    db.close()
+    
+if __name__ == "__main__":
+    mysqlconnect()
