1b617a2e03ec48dbaf8c20cf6e3c60ce450432ee
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 <giogap@gmail.com>
2022-11-18 15:15:40 -0500

    upload 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+
+    engine.connect()
+
+    metaData = MetaData()
+
+    session = Session(engine)
+
+    Base.metadata.create_all(engine)
+
+    state = session.query(State).filter(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+
+    else:
+        print('Nothing')
+
+    session.close()
