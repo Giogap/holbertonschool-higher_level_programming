63c98dc602f44a7fa6eeea7e2b46df5af2fbab56
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 <giogap@gmail.com>
2022-11-18 11:36:24 -0500

    upload 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Write a script that takes in an argument
+and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def mysqlconnect():
+    """ Function for connecting to MySQL database"""
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password='',
+        database=argv[3],
+        port=3306
+        )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = %s \
+            ORDER BY states.id ASC", (argv[4], ))
+
+    rows = cursor.fetchall()
+    for col in rows:
+        print(col)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    mysqlconnect()
