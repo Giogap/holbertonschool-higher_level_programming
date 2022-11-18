7a5753626223147d60959cfcf2afc2ae05ca133c
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 <giogap@gmail.com>
2022-11-18 13:22:41 -0500

    upload 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Write a script that lists
+all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def mysqlconnect():
+    """ Function for connecting to MySQL database"""
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password='',
+        database=argv[3],
+        port=3306
+        )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT C.id, C.name, S.name \
+            FROM states S \
+            INNER JOIN cities C \
+            ON S.id = C.state_id;")
+
+    rows = cursor.fetchall()
+    for col in rows:
+        print(col)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    mysqlconnect()
