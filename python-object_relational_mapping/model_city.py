650f8ba92704cd568a1841da6f18fe6095907786
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 <giogap@gmail.com>
2022-11-21 16:36:32 -0500

    upload 14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Doc """
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+
+class City(Base):
+    """ Class City inherith from Base
+    Implement model cities """
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True,
+                unique=True, nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'),
+                      nullable=False)
