ef51a01d5d764e76bd821b7665e37e86b88d8c30
Giogap <giogap@gmail.com>
2022-12-19 16:07:52 -0500

    upload 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,20 @@
 #!/usr/bin/node
 
+import { argv } from 'node:process';
+
+// print process.argv
+argv.forEach((val, index) => {
+    if (argv = null) {
+        console.log('No argument');
+    }
+    else if (argv = 1) {
+        console.log('Argument found');
+    }
+    else {
+        console.log('Arguments found')
+    }
+  
+});
 
 
-
-console.log(process.argv)
+console.log(process.argv);
