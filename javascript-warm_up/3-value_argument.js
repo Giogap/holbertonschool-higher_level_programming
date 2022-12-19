d55463489152398c934f80634a227eabd513a3d0
Giogap <giogap@gmail.com>
2022-12-19 18:56:57 -0500

    upload 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-let x;
-
-process.argv[x];
-
 if (process.argv[0]) {
     console.log('No argument');
 }
 else {
-    console.log(process.argv);
+    console.log(process.argv[1]);
 }
