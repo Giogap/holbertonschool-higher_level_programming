2e56c12a8b19416b1d87541e06fd6b4d9e65d392
Giogap <giogap@gmail.com>
2022-12-19 18:29:15 -0500

    upload 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,9 +4,8 @@ if (process.argv.length <= 2) {
     console.log('No argument');
   } else {
     if (process.argv.length > 3) {
-      console.log('Arguments found');
+        console.log('Arguments found');
     } else {
       console.log('Argument found');
     }
   }
-  
