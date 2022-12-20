fb4b241da8e2ddf1213e342437702750029d5b82
Giogap <giogap@gmail.com>
2022-12-20 13:58:36 -0500

    upload 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
+const n = parseInt(process.argv[2])
+
 function factorial (n) {
     if (n === 1|| isNaN(n)) {
         return 1
     }
+    return n * factorial(n - 1);
 }
-  
-
-console.log(parseInt(process.argv[2]))
+console.log(factorial(n))
