395a0ca0537e39ea814153040071f88cb27a89de
Giogap <giogap@gmail.com>
2022-12-20 16:34:08 -0500

    upload 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,11 +4,11 @@ const len = process.argv.length - 2;
 const array = process.argv.slice(2);
 
 if (len < 2 || isNaN(len)) {
-    console.log(0);
+  console.log(0);
 } else {
     array.sort(num);
     console.log(array[len - 2]);
 }
 function num (a, b) {
-    return a - b
+  return a - b;
 }
