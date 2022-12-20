f9f3a20323ffd56eaaeb8a7cc297acaeaf8618a9
Giogap <giogap@gmail.com>
2022-12-20 16:32:57 -0500

    upload 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 
 const len = process.argv.length - 2;
-const array = process.argv.slice(0)
+const array = process.argv.slice(2);
 
 if (len < 2 || isNaN(len)) {
     console.log(0);
 } else {
-    array.sort();
-    console.log(array[len])
+    array.sort(num);
+    console.log(array[len - 2]);
+}
+function num (a, b) {
+    return a - b
 }
