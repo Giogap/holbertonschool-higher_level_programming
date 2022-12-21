a9be6544dab6ffb819cef38ee8306c23e0db7e4c
Giogap <giogap@gmail.com>
2022-12-20 22:12:54 -0500

    upload 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const Sqr = require('./5-square');
+
+module.exports = class Square extends Sqr {
+  charPrint(c) {
+    if (c) {
+        for (let i = 0; i < this.height; i++) {
+            console.log('c'.repeat(this.width));
+          } 
+    } else {
+        for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+           
+  }
+};
