70d4ed2f2ebc551e18e688db79139741d79fe2b0
Giogap <giogap@gmail.com>
2022-12-21 22:17:42 -0500

    upload 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const FileName = process.argv[2];
 
-fs.readFile(FileName, 'utf8', function(err, data) {
+fs.readFile(FileName, 'utf8', function (err, data) {
   if (data) {
     console.log(data);
   } else {
