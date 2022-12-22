a5bd75c20bf2ae6d391a9ca6ece3bbac590cb0a7
Giogap <giogap@gmail.com>
2022-12-22 10:00:24 -0500

    upload 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const FileName = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(FileName, content, 'utf8', function (err) {
-    if (err) {
-        console.log(err);
-    }
+  if (err) {
+    console.log(err);
+  }
 });
