41d3d25c4cf8166cb5999635592e188990e2dd0a
Giogap <giogap@gmail.com>
2022-12-22 13:52:16 -0500

    upload 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, function(err, response) {
+ if (err) {
+   return;
+ }
+ console.log('code: ' + response.statusCode);
+});
