6e6bf2bbf4d8a5a7e2c4f1d7c6a5cc838fb14d88
Giogap <giogap@gmail.com>
2022-12-22 15:04:45 -0500

    upload 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 const moveId = process.argv[2];
 
-request.get(url + moveId, function (err, res, body) { 
+request.get(url + moveId, function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
     console.log(JSON.parse(body).title);
-    }
- });
+  }
+});
