5effe249cf7455bba263a9d4a339b3b19560454a
Giogap <giogap@gmail.com>
2022-12-22 21:51:47 -0500

    upload 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-const urlApi = process.argv[2];
+const url = process.argv[2];
 
-request(urlApi, function (err, res, body) {
-
+request.get(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const todos = JSON.parse(body);
+    const task = {};
+    for (const x of todos) {
+      if (x.completed === true) {
+        if (x.userId in task) {
+          task[x.userId]++;
+        } else {
+          task[x.userId] = 1;
+        }
+      }
+    }
+    console.log(task);
+  }
 });
